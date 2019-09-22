f02cd5486c6f2cad5db34896ff0667d6fdb451a8
ChibiAlice <55647905+ChibiAlice@users.noreply.github.com>
2019-09-22 08:29:45 +0200

    Add files via upload

---
+++ Basic Ladder Mod.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using UnityEngine;
+
+
+namespace BasicLadderMod
+{
+    // 25% increased up/down climbing speed
+
+    [HarmonyPatch(typeof(LadderConfig), "ConfigureBuildingTemplate")]
+    public class LadderConfig_ConfigureBuildingTemplate_Patch
+    {
+        public static void Postfix(GameObject go)
+        {
+            Ladder ladder = EntityTemplateExtensions.AddOrGet<Ladder>(go);
+            ladder.upwardsMovementSpeedMultiplier = 1.25f;
+            ladder.downwardsMovementSpeedMultiplier = 1.25f;
+        }
+    }
+
+
+    // Construction materials changed to refined metals
+
+    [HarmonyPatch(typeof(LadderConfig), "CreateBuildingDef")]
+    public class LadderConfig_CreateBuildingDef_Patch
+    {
+        public static void Postfix(LadderConfig __instance, BuildingDef __result)
+        {
+            __result.MaterialCategory = new string[]{"RefinedMetal"};
+        }
+    }
+
+
+}
